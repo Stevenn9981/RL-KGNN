57b2dd23062d2779c90fc87f447e9ebe003740a2
Stevenn9981 <stevenn9981@gmail.com>
2021-03-30 14:15:46 +0800

    add files

--- train_fm.py
+++ train_fm.py
@@ -43,8 +43,8 @@ def main():
         if i_episode - best_i > 4:
             break
         print("Training Meta-policy:", i_episode, "Val_Acc:", val_acc, "Avg_reward:", reward, "; Best_Acc:", best_val)
-        torch.save({'q_estimator_state_dict': agent.q_estimator.state_dict(),
-                    'target_estimator_state_dict': agent.target_estimator.state_dict(),
+        torch.save({'q_estimator_qnet_state_dict': agent.q_estimator.qnet.state_dict(),
+                    'target_estimator_qnet_state_dict': agent.target_estimator.qnet.state_dict(),
                     'Val': val_acc,
                     'Reward': reward},
                    'model/agentpoints/m-' + str(val_acc) + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '.pth.tar')

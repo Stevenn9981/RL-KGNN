af8349aa4e0695047deb3c7bbf11738a479e7abe
Stevenn9981 <stevenn9981@gmail.com>
2021-04-20 11:49:45 +0800

    add files

--- train_pretrain.py
+++ train_pretrain.py
@@ -29,13 +29,11 @@ def main():
 
     print(epochCheckpoint['Val'])
 
-
     logger1 = get_logger('log', 'logger_pre.log')
     logger2 = get_logger('log2', 'logger2_pre.log')
 
-
-    best_acc = 0.0
-    print("Training GNNs with learned meta-policy")
+    max_timesteps = 2
+    infor = '9wna_2000'
     new_env = hgnn_env(logger1, logger2, dataset=dataset)
     new_env.seed(0)
     new_env.model.load_state_dict(epochCheckpoint['state_dict'])
@@ -55,23 +53,45 @@ def main():
     best_policy.target_estimator.qnet.load_state_dict(agentCheckpoint['target_estimator_qnet_state_dict'])
 
     new_env.policy = best_policy
+    model_name = 'model_' + infor + '.pth'
 
     b_i = 0
-    best_acc = 0
+    best_val_i = 0
+    best_val_acc = 0
+    best_test_acc = 0
     actions = dict()
-    for i_episode in range(1, 20):
+    val_acc = reward = 0
+    for i_episode in range(1, 16):
         index, state = new_env.reset2()
-        for t in range(2):
-            if i_episode == 1:
+        for t in range(max_timesteps):
+            if i_episode >= 1:
                 action = best_policy.eval_step(state)
                 actions[t] = action
             state, reward, done, (val_acc, reward) = new_env.step2(logger1, logger2, index, actions[t], True)
-            logger2.info("Training GNN %d:   Val_Acc: %.5f  Reward: %.5f  " % (i_episode, val_acc, reward))
+        val_acc = new_env.eval_batch()
+        logger2.info("Training GNN %d:   Val_Acc: %.5f  Reward: %.5f  " % (i_episode, val_acc, reward))
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            if os.path.exists(model_name):
+                os.remove(model_name)
+                torch.save({'state_dict': new_env.model.state_dict(),
+                            'optimizer': new_env.optimizer.state_dict(),
+                            'Val': val_acc,
+                            'Embedding': new_env.train_data.x.weight},
+                           model_name)
+            best_val_i = i_episode
         test_acc = new_env.test_batch(logger2)
-        if test_acc > best_acc:
-            best_acc = test_acc
+        if test_acc > best_test_acc:
+            best_test_acc = test_acc
             b_i = i_episode
-        logger2.info("Training GNN %d:   Test_Acc: %.5f   Best_Acc: %.5f   Best_i: %d" % (i_episode, test_acc, best_acc, b_i))
+        logger2.info("Training GNN %d:   Test_Acc: %.5f  Best_test_i: %d  best_val_i: %d" % (
+        i_episode, test_acc, b_i, best_val_i))
+
+    logger2.info("\nStart the performance testing on test dataset:")
+    model_checkpoint = torch.load(model_name)
+    new_env.model.load_state_dict(model_checkpoint['state_dict'])
+    new_env.train_data.x = nn.Embedding.from_pretrained(model_checkpoint['Embedding'], freeze=True)
+    new_env.test_batch(logger2)
 
 
 

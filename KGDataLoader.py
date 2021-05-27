48bb66b2f67d9bf8b30852c396ac18a0d3801789
Stevenn9981 <stevenn9981@gmail.com>
2021-05-27 20:06:21 +0800

    add files

--- KGDataLoader.py
+++ KGDataLoader.py
@@ -59,7 +59,7 @@ def parse_args():
     parser.add_argument('--cf_l2loss_lambda', type=float, default=1e-5,
                         help='Lambda when calculating CF l2 loss.')
 
-    parser.add_argument('--lr', type=float, default=0.001,
+    parser.add_argument('--lr', type=float, default=0.005,
                         help='Learning rate.')
     parser.add_argument('--n_epoch', type=int, default=1000,
                         help='Number of epoch.')

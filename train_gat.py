1c65cf83870c9abf483eaeb7d29ffe0cbcb0011a
Stevenn9981 <stevenn9981@gmail.com>
2021-05-27 17:03:05 +0800

    add files

--- train_gat.py
+++ train_gat.py
@@ -61,7 +61,7 @@ def main():
 
     args = parse_args()
 
-    infor = 'gat_0.001_' + str(args.entity_dim)
+    infor = 'net_0.001_' + str(args.entity_dim)
     model_name = 'model_' + infor + '.pth'
 
     max_episodes = 100

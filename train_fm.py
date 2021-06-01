ad5b9559305c8425e591e117e4ad0bdfa264a8da
Stevenn9981 <stevenn9981@gmail.com>
2021-06-01 17:34:39 +0800

    add files

--- train_fm.py
+++ train_fm.py
@@ -61,7 +61,7 @@ def main():
 
     args = parse_args()
 
-    infor = '10wna_' + str(args.lr) + '_net_' + str(args.nd_batch_size)
+    infor = '10wna_' + str(args.lr) + '_net_0.0005_' + str(args.nd_batch_size)
     model_name = 'model_' + infor + '.pth'
 
     max_episodes = 400 // args.nd_batch_size

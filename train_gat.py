8498da379ac4d167b1d8b231b38803e2378fdff3
Stevenn9981 <stevenn9981@gmail.com>
2021-06-04 17:55:48 +0800

    add files

--- train_gat.py
+++ train_gat.py
@@ -61,7 +61,7 @@ def main():
 
     args = parse_args()
 
-    infor = 'graphsage_pretrain_' + str(args.entity_dim)
+    infor = 'net_pretrain_' + str(args.entity_dim)
     model_name = 'model_' + infor + '.pth'
 
     max_episodes = 800

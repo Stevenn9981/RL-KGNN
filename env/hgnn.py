f8d43d42c362a715a6d6bc8e4ed06d45cf8777e5
Stevenn9981 <stevenn9981@gmail.com>
2021-06-17 16:36:47 +0800

    add files

--- env/hgnn.py
+++ env/hgnn.py
@@ -142,7 +142,7 @@ class hgnn_env(object):
                 data.train_graph.edge_index[1][i].item())
         data.train_graph.adj_dist = adj_dist
         data.train_graph.attr_dict = attr_dict
-        self.etypes_lists = [[['6', '10'], ['5', '9'], ['2', '1']], [['1', '2'], ['3', '7'], ['4', '8']]]
+        self.etypes_lists = [[['6', '10'], ['2', '1']], [['1', '2'], ['4', '8']]]
 
         self.model, self.train_data = HAN(
             in_size=data.entity_dim,

e4320d5dbbf4cf83f02de3300c1f6ac3a59316c3
Stevenn9981 <stevenn9981@gmail.com>
2021-06-22 20:12:35 +0800

    add files

--- train_gat.py
+++ train_gat.py
@@ -84,7 +84,7 @@ def main():
     best = 0
     best_i = 0
     for i in range(max_episodes + 1):
-        print('Current Iter: ', i)
+        print('Current epoch: ', i)
         if i % 40 == 0:
             # env.eval_batch(100)
             acc = env.test_batch(logger2)

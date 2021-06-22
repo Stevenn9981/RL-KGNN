cc1c32a8d29e66dbe65b244da164e5c250deca7c
Stevenn9981 <stevenn9981@gmail.com>
2021-06-22 19:52:43 +0800

    add files

--- env/hgnn.py
+++ env/hgnn.py
@@ -252,26 +252,26 @@ class hgnn_env(object):
     def get_all_user_embedding(self):
         user_emb = torch.tensor([]).to(self.device)
         all_user_ids = torch.tensor(range(self.train_data.x[self.data.node_type_list == 4].shape[0]))
-        batch_ids = torch.split(all_user_ids, self.args.kg_batch_size)
+        # batch_ids = torch.split(all_user_ids, self.args.kg_batch_size)
         for metapaths in self.etypes_lists:
             start_type = self.train_data.e_n_dict[metapaths[0][0]][0]
             if start_type == 4:
-                for b_ids in batch_ids:
-                    emb = self.get_user_embedding(b_ids)
-                    user_emb = torch.cat([user_emb, emb], 0)
+                # for b_ids in batch_ids:
+                user_emb = self.get_user_embedding(all_user_ids)
+                    # user_emb = torch.cat([user_emb, emb], 0)
         return user_emb
 
 
     def get_all_item_embedding(self):
         item_emb = torch.tensor([]).to(self.device)
         all_item_ids = torch.tensor(range(self.train_data.x[self.data.node_type_list == 0].shape[0]))
-        batch_ids = torch.split(all_item_ids, self.args.kg_batch_size)
+        # batch_ids = torch.split(all_item_ids, self.args.kg_batch_size)
         for metapaths in self.etypes_lists:
             start_type = self.train_data.e_n_dict[metapaths[0][0]][0]
             if start_type == 0:
-                for b_ids in batch_ids:
-                    emb = self.get_item_embedding(b_ids)
-                    item_emb = torch.cat([item_emb, emb], 0)
+                # for b_ids in batch_ids:
+                item_emb = self.get_item_embedding(all_item_ids)
+                    # item_emb = torch.cat([item_emb, emb], 0)
         return item_emb
 
     def _set_action_space(self, _max):

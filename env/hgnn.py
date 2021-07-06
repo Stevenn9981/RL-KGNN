d638405667abc010a8f542f34fe72e333b2968a1
Stevenn9981 <stevenn9981@gmail.com>
2021-07-06 17:05:17 +0800

    add files

--- env/hgnn.py
+++ env/hgnn.py
@@ -177,7 +177,7 @@ class hgnn_env(object):
                              out_size=num_classes,
                              num_heads=args.num_heads,
                              dropout=0.6).to(self.device)
-            self.embedding_func = nn.Linear(num_classes, 32)
+            self.embedding_func = nn.Linear(num_classes, 32).to(self.device)
             self._set_action_space(3)
             self.policy = None
 

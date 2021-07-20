5676b1ef8cc8ec12050740d484cf17e6a787f8d6
Stevenn9981 <stevenn9981@gmail.com>
2021-07-20 16:57:37 +0800

    add files

--- train_with_agent.py
+++ train_with_agent.py
@@ -134,15 +134,14 @@ def main():
     item_state = env.item_reset()
     mp_set = []
     for i_episode in range(max_timesteps):
-        A = best_user_policy.predict_batch(user_state)
-        user_action = np.random.choice(np.arange(len(A)), p=A, size=user_state.shape[0])
-        A = best_user_policy.predict_batch(item_state)
-        item_action = np.random.choice(np.arange(len(A)), p=A, size=item_state.shape[0])
+        # A = best_user_policy.predict_batch(user_state)
+        # user_action = np.random.choice(np.arange(len(A)), p=A, size=user_state.shape[0])
+        # A = best_user_policy.predict_batch(item_state)
+        # item_action = np.random.choice(np.arange(len(A)), p=A, size=item_state.shape[0])
 
-        # user_action = best_user_policy.eval_step(user_state)
-        # item_action = best_user_policy.eval_step(item_state)
+        user_action = best_user_policy.eval_step(user_state)
 
-        env.model.reset()
+        # env.model.reset()
         user_state, _, user_done, (val_acc, _) = env.user_step(logger1, logger2, user_action, True)
 
         # val_acc = new_env.eval_batch(100)
@@ -155,7 +154,10 @@ def main():
         logger2.info("Evaluating GNN %d:   Val_Acc: %.5f  Reward: %.5f  best_val_i: %d" % (
         i_episode, val_acc, reward, best_val_i))
 
-        env.model.reset()
+    env.etypes_lists = mp_set
+    for i_episode in range(max_timesteps):
+        item_action = best_user_policy.eval_step(item_state)
+        # env.model.reset()
         item_state, _, item_done, (val_acc, _) = env.item_step(logger1, logger2, item_action, True)
         logger2.info("Meta-path set: %s" % (str(env.etypes_lists)))
         print("Meta-path set: %s" % (str(env.etypes_lists)))

02460e75392191947a60fca23608af425a8e480b
Stevenn9981 <stevenn9981@gmail.com>
2021-07-25 17:29:47 +0800

    add files

--- dqn_agent_pytorch.py
+++ dqn_agent_pytorch.py
@@ -176,8 +176,6 @@ class DQNAgent(object):
 
     def user_learn(self, logger1, logger2, env, total_timesteps):
         next_state_batch = env.user_reset()
-        import pdb
-        pdb.set_trace()
         for t in range(total_timesteps):
             A = self.predict_batch(next_state_batch)
             best_actions = np.random.choice(np.arange(len(A)), p=A, size=next_state_batch.shape[0])

46e44b91cf556ab768b9e18ff27cc2d2bd44bf82
Stevenn9981 <stevenn9981@gmail.com>
2021-08-02 00:23:02 +0800

    add files

--- dqn_agent_pytorch.py
+++ dqn_agent_pytorch.py
@@ -194,6 +194,7 @@ class DQNAgent(object):
         next_state_batch = env.item_reset()
         for t in range(total_timesteps):
             A = self.predict_batch(next_state_batch)
+            logger2.info("Eval Step: %d" % self.eval_step(next_state_batch))
             best_actions = np.random.choice(np.arange(len(A)), p=A, size=next_state_batch.shape[0])
             state_batch = next_state_batch
             next_state_batch, reward_batch, done_batch, debug = env.item_step(logger1, logger2,
@@ -426,6 +427,9 @@ class Estimator(object):
 
         # (batch, action_num) -> (batch, )
         Q = torch.gather(q_as, dim=-1, index=a.unsqueeze(-1)).squeeze(-1)
+
+        print(Q)
+        print(y)
 
         # update model
         batch_loss = self.mse_loss(Q, y)

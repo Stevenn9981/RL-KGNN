db191ce97cad89007d4fa19be5676881c300a9cd
Stevenn9981 <stevenn9981@gmail.com>
2021-08-04 11:34:08 +0800

    add files

--- train_fm.py
+++ train_fm.py
@@ -71,7 +71,7 @@ def main():
     model_name = 'model_' + infor + '.pth'
 
     u_max_episodes = 15
-    i_max_episodes = 15
+    i_max_episodes = 20
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     logger1 = get_logger('log', 'logger_' + infor + '.log')

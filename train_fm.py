eb08a5e3a1b5554c50a7468214c1c83bd33d358f
Wentao Ning <stevenn9981@gmail.com>
2021-09-18 17:36:19 +0800

    modify

--- train_fm.py
+++ train_fm.py
@@ -95,8 +95,8 @@ def main():
                           norm_step=1,
                           batch_size=1,
                           state_shape=env.obs.shape,
-                          mlp_layers=[32, 32],
-                          learning_rate=0.01,
+                          mlp_layers=[32, 64, 32],
+                          learning_rate=0.005,
                           device=torch.device(device)
                           )
 
@@ -107,8 +107,8 @@ def main():
                           norm_step=1,
                           batch_size=1,
                           state_shape=env.obs.shape,
-                          mlp_layers=[32, 32],
-                          learning_rate=0.01,
+                          mlp_layers=[32, 64, 32],
+                          learning_rate=0.005,
                           device=torch.device(device)
                           )
 
